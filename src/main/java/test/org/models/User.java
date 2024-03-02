78b6ebc1325b6b4e487336dd887710957d269934
SommersetMoem <nnekit10@mail.ru>
2024-03-02 09:55:27 +0300

    add Utils

--- src/main/java/test/org/models/User.java
+++ src/main/java/test/org/models/User.java
@@ -1,26 +1,9 @@
 package test.org.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class User {
     private String login;
     private String password;
-    private String lastName;
-    private String firstName;
-    private String surName;
-    private String birthDate;
-    private String sex;
-    private String email;
-    private String phone;
-
-    public User(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public User() {
-    }
 }
--- src/test/java/ui/Test_1.java
+++ src/test/java/ui/Test_1.java
@@ -2,9 +2,6 @@ package ui;
 
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class Test_1 extends BaseSelenideTest {
 
     @Test
